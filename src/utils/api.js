f917af4db9ac221a4091d21b601ed60898c29d7a
John Judeh <johnjudeh@hotmail.com>
2020-05-11 23:20:11 +0100

    feat: Create a client to communicate with the mocked db _DATA.js, fix incorrect import of App.js and add avatars

---
+++ src/utils/api.js
@@ -0,0 +1,34 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer,
+} from './_DATA';
+
+export function getInitialData() {
+    return Promise.all([
+        _getUsers(),
+        _getQuestions(),
+    ]).then(([users, questions]) => {
+        return {
+            users,
+            questions,
+        }
+    });
+}
+
+export function saveQuestion(optionOneText, optionTwoText, author) {
+    return _saveQuestion({
+        optionOneText,
+        optionTwoText,
+        author,
+    });
+}
+
+export function saveQuestionAnswer(authedUser, qid, answer) {
+    return _saveQuestionAnswer({
+        authedUser,
+        qid,
+        answer,
+    });
+}
