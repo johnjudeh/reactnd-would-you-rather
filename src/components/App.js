b9cd2c27d636ab20990f8c8b8ea6fd891c2d7e52
John Judeh <johnjudeh@hotmail.com>
2020-05-11 23:35:55 +0100

    feat: Create store and its initial reducers. Pass the store down to all components through the react-redux Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import reducer from '../reducers';
+import { Provider } from 'react-redux';
 
-function App() {
-    return (
-        <div>
-            <h1>Would You Rather...</h1>
-        </div>
-    );
+const store = createStore(reducer);
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <h1>Would You Rather...</h1>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
