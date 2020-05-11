0d35555da58552797b16b889fc0d33e0ee9077c5
John Judeh <johnjudeh@hotmail.com>
2020-05-11 23:47:40 +0100

    fix: Move store creation to index.js rather than the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
-import reducer from '../reducers';
-import middleware from '../middleware';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducer, middleware);
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <div>
-                    <h1>Would You Rather...</h1>
-                </div>
-            </Provider>
+            <div>
+                <h1>Would You Rather...</h1>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
+import reducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
